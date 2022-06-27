963ceaff33773fe761e6c0db8ed0858dd4a42496
buingoclam <lb130801@gmail.com>
2022-06-27 16:14:17 +0700

    push code

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,122 @@
+<footer class="text-center text-lg-start text-white pt-5">
+<div class="footer">
+        <div class=" text-center text-md-start ">
+            <div class="row footer-content">
+                <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                    <div class="footer-logo " href="#">
+                        <img  src="{{ asset('images/hapo_white.png') }}" alt="HapoLearn Logo">
+                    </div>
+                    <p class="slogan-company">
+                        Interactive lessons, "on-the-go" practice, peer support
+                    </p>
+                </div>
+
+                <div class="col-md-2 col-lg-1 col-xl-1 mx-auto">
+                    <p>
+                        <a href="#!" class="text-reset">Home</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">List course</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">List lesson</a>
+                    </p>
+                   
+                </div>
+                <div class="col-md-2 col-lg-1 col-xl-1 mx-auto p-0">
+                    <p>
+                        <a href="#!" class="text-reset">Lesson detail</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">Blog</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">Profile</a>
+                    </p>
+                </div>
+
+                <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4 ">
+                   <div class="list-icon">
+                     <p class="icon-item">
+                        <a href=""><i class="fa-brands fa-facebook"></i></a>
+                    </p>
+                    <p class="icon-item">
+                        <a href=""><i class="fa-solid fa-envelope"></i></a>
+                    </p>
+                    <p class="icon-item">
+                        <a href=""><i class="fa-solid fa-phone"></i></a>
+                    </p>   
+                   </div>
+                   <div class="list-address">
+                    <p>
+                        <a href="">
+                            facebook.com/tuyen.dung.haposoft
+                        </a> 
+                    </p>
+                   
+                   </div>
+                   
+                       
+                   
+                </div>
+        </div>
+    </div>
+</div>
+<div class="copyright">
+     © 2020 HapoLearn, Inc. All rights reserved.
+</div>
+
+<div class="footer-mobie">
+    <div class="footer-mobie-content">
+        <div class="w-50">
+            <p>
+                <a href="#!" class="text-reset">Home</a>
+            </p>
+            <p>
+                <a href="#!" class="text-reset">List course</a>
+            </p>
+            <p>
+                <a href="#!" class="text-reset">List lesson</a>
+            </p>
+        </div>
+
+        <div class="w-50">
+            <p>
+                <a href="#!" class="text-reset">Lesson detail</a>
+            </p>
+            <p>
+                <a href="#!" class="text-reset">Login/Register</a>
+            </p>
+            <p>
+                <a href="#!" class="text-reset">Profile</a>
+            </p>
+        </div>
+        <div class="w-50">
+            <div class="footer-logo " href="#">
+                <img  src="{{ asset('images/hapo_white.png') }}" alt="HapoLearn Logo">
+            </div>
+            <p class="slogan-company">
+                Interactive lessons, "on-the-go" practice, peer support
+            </p>
+        </div>
+        <div class="w-50">
+            <div class="list-icon">
+                <p class="icon-item">
+                   <a href=""><i class="fa-brands fa-facebook"></i></a>
+               </p>
+               <p class="icon-item">
+                   <a href=""><i class="fa-solid fa-envelope"></i></a>
+               </p>
+               <p class="icon-item">
+                   <a href=""><i class="fa-solid fa-phone"></i></a>
+               </p>   
+            </div>
+        </div>
+    </div>
+    
+    <div class="clear"></div>
+    <div class="copyright">
+     © 2020 HapoLearn, Inc. All rights reserved.
+    </div>
+</div>
+</footer>
